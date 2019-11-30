803fbc25511890ef163bc84b89926f4eb9ca3947
AmasingCoconut <58276535+AmasingCoconut@users.noreply.github.com>
2019-11-30 18:25:31 +0300

    Add project files.

---
+++ C2HW2/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2HW2
+{
+    abstract class Employee
+    {
+        protected double salary;
+        public Employee(double salary)
+        {
+            this.salary = salary;
+        }
+
+        public abstract double Salary();
+    }
+}
---
+++ C2HW2/EmployeeFixed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2HW2
+{
+    class EmployeeFixed : Employee
+    {
+        public EmployeeFixed(double salary) :base(salary)
+        {
+        }
+
+        public override double Salary()
+        {
+            return salary;
+        }
+    }
+}
